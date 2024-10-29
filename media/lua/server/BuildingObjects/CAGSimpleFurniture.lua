509be9b2e90958137fa6c1a9bd94640231d18fd5
Daniel Krois <danielkrois@hotmail.com>
2024-10-29 16:37:12 +0100

    added shortblund skill to simpleFurniture

---
+++ media/lua/server/BuildingObjects/CAGSimpleFurniture.lua
@@ -0,0 +1,16 @@
+function ISSimpleFurniture:new(name, sprite, northSprite)
+	local o = {};
+	setmetatable(o, self);
+	self.__index = self;
+	o:init();
+	o:setSprite(sprite);
+	o:setNorthSprite(northSprite);
+	o.name = name;
+	o.canBarricade = false;
+	o.dismantable = true;
+	o.blockAllTheSquare = true;
+	o.canBeAlwaysPlaced = true;
+	o.buildLow = true;
+    o.modData['xp:SmallBlunt'] = 1;
+	return o;
+end
